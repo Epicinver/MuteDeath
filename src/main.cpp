8c481087d4490166fea8bed570807204e6ac04fc
Butter <arranh2302@hotmail.com>
2025-12-17 16:17:25 +0000

    vibe coded example of a menu so i can see what im working with

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,67 @@ public:
 
 	void onModSettings(CCObject *)
 	{
-		// the stinky menu
-		log::info("stinky mod settings clicked");
+		log::info("mod settings clicked");
+
+		// Menu
+		CCDirector::sharedDirector()
+			->getRunningScene()
+			->addChild(ModSettingsLayer::create(), 999);
+	}
+};
+
+class ModSettingsLayer : public CCLayer
+{
+public:
+	static ModSettingsLayer *create()
+	{
+		auto ret = new ModSettingsLayer();
+		if (ret && ret->init())
+		{
+			ret->autorelease();
+			return ret;
 		}
+		CC_SAFE_DELETE(ret);
+		return nullptr;
+	}
+
+	bool init()
+	{
+		if (!CCLayer::init())
+			return false;
+
+		this->setTouchEnabled(true);
+		this->setKeypadEnabled(true);
+
+		auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+		// dark background
+		auto bg = CCLayerColor::create({0, 0, 0, 160});
+		this->addChild(bg);
+
+		// menu container
+		auto menu = CCMenu::create();
+		menu->setPosition({winSize.width / 2, winSize.height / 2});
+		this->addChild(menu);
+
+		// close button (round style)
+		auto circle = CCSprite::createWithSpriteFrameName("GJ_fxOnBtn_001.png");
+		auto closeBtn = CCMenuItemSpriteExtra::create(
+			circle,
+			this,
+			menu_selector(ModSettingsLayer::onClose));
+
+		auto xIcon = CCSprite::createWithSpriteFrameName("GJ_fxOnBtn_001.png");
+		xIcon->setPosition(circle->getContentSize() / 2);
+		circle->addChild(xIcon);
+
+		menu->addChild(closeBtn);
+
+		return true;
+	}
+
+	void onClose(CCObject *)
+	{
+		this->removeFromParentAndCleanup(true);
+	}
 };
